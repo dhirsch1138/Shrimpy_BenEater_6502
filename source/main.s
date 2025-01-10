fbeb8d344d3925a470639c9a8875e8f5000b1c3b
David Hirsch <dhirsch1138@gmail.com>
2025-01-10 23:55:38 +0000

    broken. but saving progress

--- source/main.s
+++ source/main.s
@@ -2,11 +2,17 @@
 ;
 ;Includes
   .include "via.s_imports"
-  .include "lcd.s_imports"
+  ;.include "lcd.s_imports"
+
+LCD_4BIT_E  = %01000000
+LCD_4BIT_RW = %00100000
+LCD_4BIT_RS = %00010000 
   
   .code
 
 reset:
+  ldx #$ff
+  txs
   jsr lcd_init
   lda #%00101000 ; Set 4-bit mode; 2-line display; 5x8 font
   jsr lcd_instruction
@@ -16,47 +22,29 @@ reset:
   jsr lcd_instruction
   lda #%00000001 ; Clear display
   jsr lcd_instruction
+  ldx #0
 
-printfun:
-  lda #"I"
+print:
+  lda message,x
+  beq loop
   jsr print_char
-  lda #" "
-  jsr print_char
-  lda #"l"
-  jsr print_char
-  lda #"o"
-  jsr print_char
-  lda #"v"
-  jsr print_char
-  lda #"e"
-  jsr print_char
-  lda #" "
-  jsr print_char
-  lda #"J"
-  jsr print_char
-  lda #"a"
-  jsr print_char
-  lda #"c"
-  jsr print_char
-  lda #"k"
-  jsr print_char
-  lda #"i"
-  jsr print_char
-  lda #"e"
-  jsr print_char
+  inx
+  jmp print
 
 loop:
   nop
   ;jump back to the loop reference. We are now looping forever and ever and ever
   jmp loop
 
+message: .asciiz "Jackie is cute!"
+
 lcd_init:
   lda #%00000010 ; Set 4-bit mode
-  sta PORTB
+  sta VIA_PORTB
   ora #LCD_4BIT_E
-  sta PORTB
+  sta VIA_PORTB
   and #%00001111
-  sta PORTB
+  sta VIA_PORTB
   rts
 
 lcd_instruction:
@@ -66,45 +54,45 @@ lcd_instruction:
   lsr
   lsr
   lsr            ; Send high 4 bits
-  sta PORTB
+  sta VIA_PORTB
   ora #LCD_4BIT_E        ; Set E bit to send instruction
-  sta PORTB
+  sta VIA_PORTB
   eor #LCD_4BIT_E         ; Clear E bit
-  sta PORTB
+  sta VIA_PORTB
   pla
   and #%00001111 ; Send low 4 bits
-  sta PORTB
+  sta VIA_PORTB
   ora #LCD_4BIT_E         ; Set E bit to send instruction
-  sta PORTB
+  sta VIA_PORTB
   eor #LCD_4BIT_E         ; Clear E bit
-  sta PORTB
+  sta VIA_PORTB
   rts
 
 
 lcd_wait:
   pha
   lda #%11110000  ; LCD data is input
-  sta DDRB
+  sta VIA_DDRB
 lcdbusy:
   lda #LCD_4BIT_RW
-  sta PORTB
+  sta VIA_PORTB
   lda #(LCD_4BIT_RW | LCD_4BIT_E)
-  sta PORTB
-  lda PORTB       ; Read high nibble
+  sta VIA_PORTB
+  lda VIA_PORTB       ; Read high nibble
   pha             ; and put on stack since it has the busy flag
   lda #LCD_4BIT_RW
-  sta PORTB
+  sta VIA_PORTB
   lda #(LCD_4BIT_RW | LCD_4BIT_E)
-  sta PORTB
-  lda PORTB       ; Read low nibble
+  sta VIA_PORTB
+  lda VIA_PORTB       ; Read low nibble
   pla             ; Get high nibble off stack
   and #%00001000
   bne lcdbusy
 
   lda #LCD_4BIT_RW
-  sta PORTB
+  sta VIA_PORTB
   lda #%11111111  ; LCD data is output
-  sta DDRB
+  sta VIA_DDRB
   pla
   rts
 
@@ -115,18 +103,18 @@ print_char:
   lsr
   lsr
   lsr             ; Send high 4 bits
-  ora #RS         ; Set RS
-  sta PORTB
-  ora #E          ; Set E bit to send instruction
-  sta PORTB
-  eor #E          ; Clear E bit
-  sta PORTB
+  ora #LCD_4BIT_RS         ; Set RS
+  sta VIA_PORTB
+  ora #LCD_4BIT_E          ; Set E bit to send instruction
+  sta VIA_PORTB
+  eor #LCD_4BIT_E          ; Clear E bit
+  sta VIA_PORTB
   pla
   and #%00001111  ; Send low 4 bits
-  ora #RS         ; Set RS
-  sta PORTB
-  ora #E          ; Set E bit to send instruction
-  sta PORTB
-  eor #E          ; Clear E bit
-  sta PORTB
+  ora #LCD_4BIT_RS         ; Set RS
+  sta VIA_PORTB
+  ora #LCD_4BIT_E          ; Set E bit to send instruction
+  sta VIA_PORTB
+  eor #LCD_4BIT_E          ; Clear E bit
+  sta VIA_PORTB
   rts
