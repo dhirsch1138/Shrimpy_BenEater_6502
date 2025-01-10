8f985996886bc5ce5a1fa7fa0a4189d00bbe2343
David Hirsch <dhirsch1138@gmail.com>
2025-01-10 22:53:25 +0000

    Brought over LCD functions into main code segment, renamed blink.s to main.s to make it more general

---
+++ source/lcd.s
@@ -0,0 +1,11 @@
+;PURPOSE - defines the static register references & lcd functions 
+;  interface as provided by Ben Eater's videos https://eater.net/6502
+
+
+.export LCD_4BIT_E
+.export LCD_4BIT_RW
+.export LCD_4BIT_RS
+
+E  = %01000000
+RW = %00100000
+RS = %00010000
